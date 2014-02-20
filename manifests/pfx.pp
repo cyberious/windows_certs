c5e4e84200372fcfecb87af5757f1c5742022a7c
Travis Fields <tfields@commercehub.com>
2014-02-20 13:53:31 -0500

    updated fixtures and pfx to look for server 2012

--- manifests/pfx.pp
+++ manifests/pfx.pp
@@ -13,12 +13,21 @@ define windows_certs::pfx ($cert_name = $title,$cert_stage_location,$cert_secure
       source  => $source_location,
       ensure  => file
   }
-  if($::kernelmajversion == "6.2"){
-    $cmd = "Import-Module WebAdministration; Import-PfxCertificate -FilePath ${$cert_stage_location} -CertStoreLocation Cert:\\LocalMachine\\${cert_store} -Password (\"${$cert_secure_pass}\" | ConvertTo-SecureString -Key @(1..32))"
-    $checkCmd = "Import-Module WebAdministration; if (gci cert:\LocalMachine\${cert_store} -R | where FriendlyName -eq \"${cert_name}\") { exit 1 } else { exit 0 }"
+  $checkCmd = "Import-Module WebAdministration; if (gci cert:\LocalMachine\${cert_store} -R | where FriendlyName -eq \"${cert_name}\") { exit 1 } else { exit 0 }"
 
+if($::kernelmajversion == "6.2"){
+    $cmd = "Import-Module WebAdministration; Import-PfxCertificate -FilePath ${cert_stage_location} -CertStoreLocation Cert:\\LocalMachine\\${cert_store} -Password (\"${cert_secure_pass}\" | ConvertTo-SecureString -Key @(1..32))"
   }else{
-
+    $functionToImport = 'function Import-PfxCertificate {
+      param([String]$FilePath,[String]$certRootStore = “LocalMachine”,[String]$certStore = “WebHosting”,$Password = $null)
+      $pfx = new-object System.Security.Cryptography.X509Certificates.X509Certificate2
+      $pfx.import($FilePath,$Password,“Exportable,PersistKeySet”)
+      $store = new-object System.Security.Cryptography.X509Certificates.X509Store($certStore,$certRootStore)
+      $store.open(“MaxAllowed”)
+      $store.add($pfx)
+      $store.close()
+    }'
+    $cmd = "$functionToImport;Import-PfxCertificate -FilePath ${cert_stage_location} -CertStore ${cert_store} -Password (\"${cert_secure_pass}\" | ConvertTo-SecureString -Key @(1..32))"
   }
   exec{"Insert-PFX-Cert-${cert_name}":
       command   => $cmd,
